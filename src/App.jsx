7d525f272cbd3118d4a4c24713d92e116b6968f1
kopasovska <v.kopasovska@gmail.com>
2025-04-28 16:40:40 +0200

    Add vercel config

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,33 @@
 import { useState } from 'react';
-import './App.css';
+import css from './App.module.css';
+import { NavLink, Route, Routes } from 'react-router-dom';
+import clsx from 'clsx';
+
+import HomePage from './pages/HomePage/HomePage';
+// import MoviesPage from './pages/MoviesPage/MoviesPage';
+// import MovieDetailsPage from './pages/MovieDetailsPage/MovieDetailsPage';
+// import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
+
+const buildLinkClass = ({ isActive }) => {
+  return clsx(css.link, isActive && css.active);
+};
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <nav className={css.nav}>
+        <NavLink to="/" className={buildLinkClass}>
+          Home
+        </NavLink>
+      </nav>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        {/* <Route path="/movies" element={<MoviesPage />} />
+        <Route path="/movies/:movieId" element={<MovieDetailsPage />} />
+        <Route path="*" element={<NotFoundPage />} /> */}
+      </Routes>
+    </>
+  );
 }
 
 export default App;
